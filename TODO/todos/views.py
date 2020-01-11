d29bbbe809105165c3df5c224b38f029699e2324
captbilard <bilardosinulu@gmail.com>
2020-01-12 00:56:38 +0100

    Created index template

--- TODO/todos/views.py
+++ TODO/todos/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, redirect
 from .forms import TodoForm
-from django.views import View
+from django.views import View, generic
+from django.shortcuts import get_object_or_404
+from .models import Todo
 # Create your views here.
+
+
 class AddTodoView(View):
     form_class = TodoForm
     template_name = "todo/index.html"
 
     def get(self, request, *args, **kwargs):
         form = self.form_class()
-        return render(request, self.template_name, {'form':form})
+        todos = Todo.objects.all()
+        return render(request, self.template_name, {'form':form, 'todos':todos})
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
@@ -16,3 +21,17 @@ class AddTodoView(View):
             form.save()
             return redirect('add-todo')
         return render(request, self.template_name, {'form':form})
+
+def completed(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    todo.completed = True
+    todo.save()
+    return redirect('add-todo')
+
+def deleteCompleted(request):
+    Todo.objects.get(completed__exact=True).delete()
+    return redirect('add-todo')
+
+def deleteAll(request):
+    Todo.objects.all().delete()
+    return redirect('add-todo')
